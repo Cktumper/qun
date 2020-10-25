0a3972dc704503297bc78b812ec7099ea7d0860a
xjqxz <xjqxz_2@163.com>
2020-10-26 00:45:42 +0800

    WEBSOCKET 升级连接

---
+++ services/websocket/controllers/index.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"sync"
+)
+
+type IndexController struct{}
+
+var (
+	indexInstance     *IndexController
+	indexOnceInstance sync.Once
+)
+
+//	创建一个版本号控制器
+//
+//	Author(Wind)
+func NewIndexController() *IndexController {
+	indexOnceInstance.Do(func() {
+		indexInstance = &IndexController{}
+	})
+
+	return indexInstance
+}
+
+//	群聊器首页路由
+//
+//	Author(Wind)
+func (p *IndexController) HTML(c *gin.Context) {
+	c.HTML(http.StatusOK, "wss.html", gin.H{})
+}
